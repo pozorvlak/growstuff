98b85b53f0575c9fe96b1a53ec65587f5fe48c77
Miles Gould <mgould@redhat.com>
2016-05-18 16:30:40 +0100

    Kinda working, but PhantomJS keeps crashing

--- script/build-env.sh
+++ script/build-env.sh
@@ -1,8 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-/usr/share/rvm/bin/rvm install ruby-2.1.8
+read RUBY_VERSION < .ruby-version
+read RUBY_GEMSET < .ruby-gemset
+
+. $(dirname $0)/rvm_config.sh
+rvm install --quiet-curl ruby-$RUBY_VERSION
+rvm gemset use $RUBY_GEMSET
 gem install bundler
 bundle install
 rake db:create
 rake db:migrate
 rake db:test:prepare
+
+echo <<INSTRUCTIONS
+All done! To check everything's OK, run
+
+  rvm gemset use $RUBY_GEMSET
+  rake
+
+INSTRUCTIONS
